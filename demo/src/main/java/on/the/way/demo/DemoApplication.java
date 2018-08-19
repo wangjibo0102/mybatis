0a494f3c422dc320e9ecf425e0c9a16b409aae24
Administrator <Administrator@XZ63HQCZ8XCZ9D6>
2018-08-20 00:23:02 +0800

    文件提交

--- demo/src/main/java/on/the/way/demo/DemoApplication.java
+++ demo/src/main/java/on/the/way/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package on.the.way.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
 
